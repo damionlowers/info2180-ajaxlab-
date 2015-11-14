dc48f2fa40afcd98ed5a6b2a941247e40db56fdb
Damion Lowers <damion.lowers@gmail.com>
2015-11-14 17:50:54 -0500

    initial commit

--- request.php
+++ request.php
@@ -3,16 +3,42 @@
 
 # accept a term (keyword)
 # respond with a value
-$query = $_REQUEST['q'];
+// $query = $_REQUEST['q'];
 
-$definition = [
-    "definition" => "a statement of the exact meaning of a word, especially in a dictionary.",
-    "bar" => "a place that sells alcholic beverages",
-    "ajax" => "technique which involves the use of javascript and xml"
-];
+// $definition = [
+//     "definition" => "a statement of the exact meaning of a word, especially in a dictionary.",
+//     "bar" => "a place that sells alcholic beverages",
+//     "ajax" => "technique which involves the use of javascript and xml"
+// ];
 
-print $query;
-print "<br>";
-print($definition[$query]);
+// print $query;
+// print "<br>";
+// print($definition[$query]);
+// // print $definition['definition']
+
+header("Content-type: text/xml");
+print("");
+$string = <<<XML
+<ol>
+	<li>
+		<definition>
+			definition = a statement of the exact meaning of a word, especially in a dictionary.
+		</definition>
+	</li>
+	<li>
+		<definition>
+			bar = a place that sells alcholic beverages.
+		</definition>
+	</li>
+	<li>
+		<definition>
+			ajax = technique which involves the use of javascript and xml.
+		</definition>
+	</li>
+  </ol>
+XML;
+$xml = new SimpleXMLElement($string);
+echo $xml->asXML();
+
 
 ?>
