b4f61d834f77f96fe53cbde43a8044a47b0e4123
Damion Lowers <damion.lowers@gmail.com>
2015-11-14 22:51:21 -0500

    Update to searchbutton.html

--- request.php
+++ request.php
@@ -24,16 +24,25 @@ $string = <<<XML
 		<definition>
 			definition = a statement of the exact meaning of a word, especially in a dictionary.
 		</definition>
+		<authors>
+			<p>Author: <h2>Damion Lowers</h2></p>
+		</authors>
 	</li>
 	<li>
 		<definition>
 			bar = a place that sells alcholic beverages.
 		</definition>
+		<authors>
+			<p>Author: <h2>Damion Lowers</h2></p>
+		</authors>
 	</li>
 	<li>
 		<definition>
 			ajax = technique which involves the use of javascript and xml.
 		</definition>
+		<authors>
+			<p>Author: <h2>Damion Lowers</h2></p>
+		</authors>
 	</li>
   </ol>
 XML;
